ec557c79d853b38c82b4fad5db53091fd317bc05
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2023-07-07 09:44:23 +0000

    Revert " Excluding containers from sync with RegExp"
    This reverts commit 536e3dce2fea1e610eebb492d49504323cbecf9b.

--- src/js/options.js
+++ src/js/options.js
@@ -115,13 +115,10 @@ async function setupOptions() {
   const { syncEnabled } = await browser.storage.local.get("syncEnabled");
   const { replaceTabEnabled } = await browser.storage.local.get("replaceTabEnabled");
   const { currentThemeId } = await browser.storage.local.get("currentThemeId");
-  const { syncExcludeRegExp } = await browser.storage.local.get("syncExcludeRegExp");
 
   document.querySelector("#syncCheck").checked = !!syncEnabled;
   document.querySelector("#replaceTabCheck").checked = !!replaceTabEnabled;
   document.querySelector("#changeTheme").selectedIndex = currentThemeId;
-  document.querySelector("#syncExcludeRegExp").value = syncExcludeRegExp || "";
-  
   setupContainerShortcutSelects();
 }
 
@@ -175,15 +172,10 @@ async function resetPermissionsUi() {
 browser.permissions.onAdded.addListener(resetPermissionsUi);
 browser.permissions.onRemoved.addListener(resetPermissionsUi);
 
-function updateSyncExcludeRegExp(event) {
-  browser.storage.local.set({syncExcludeRegExp: event.target.value});
-}
-
 document.addEventListener("DOMContentLoaded", setupOptions);
 document.querySelector("#syncCheck").addEventListener( "change", enableDisableSync);
 document.querySelector("#replaceTabCheck").addEventListener( "change", enableDisableReplaceTab);
 document.querySelector("#changeTheme").addEventListener( "change", changeTheme);
-document.querySelector("#syncExcludeRegExp").addEventListener( "change", updateSyncExcludeRegExp);
 document.querySelector("#containersRestoreInput").addEventListener( "change", restoreContainers);
 
 maybeShowPermissionsWarningIcon();
